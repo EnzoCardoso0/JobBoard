17fa298dc794651bf42d84ed1c3228de6de24f4f
Enzo Cardoso <enzo.cardoso@epitech.eu>
2022-10-16 12:44:25 +0200

    Avancement project

--- migrations/Version20221014102647.php
+++ migrations/Version20221014102647.php
@@ -19,6 +19,7 @@ final class Version20221014102647 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE advertissement (id INT AUTO_INCREMENT NOT NULL, id_owner_id INT NOT NULL, job VARCHAR(100) NOT NULL, job_desc VARCHAR(500) DEFAULT NULL, date VARCHAR(20) NOT NULL, INDEX IDX_5E225D2F2EE78D6C (id_owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, id_ad_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', name VARCHAR(40) NOT NULL, cp VARCHAR(5) NOT NULL, adress VARCHAR(60) NOT NULL, tel VARCHAR(10) NOT NULL, is_compagny TINYINT(1) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649A1B194A6 (id_ad_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
